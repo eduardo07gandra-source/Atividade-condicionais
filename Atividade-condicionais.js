c29117bd7d6487da38bc4d63082fd15623944e50
EDUARDO GANDRA <eduardo07gandra@gmail.com>
2026-02-23 14:33:13 -0300

    Add files via upload

---
+++ Atividade-condicionais.js
@@ -0,0 +1,162 @@
+//Exercicios 1
+
+let numero1 = 10;
+
+if (numero1 >= 0) {
+  console.log("Número positivo");
+} else {
+  console.log("Número negativo");
+}
+
+//Exercicio 2
+
+let idade1 = 20;
+
+if (idade1 >= 18) {
+  console.log("Maior de idade");
+} else {
+  console.log("Menor de idade");
+}
+
+//Exercicio 3
+
+let numero = 7;
+
+if (numero % 2 === 0) {
+  console.log("Número par");
+} else {
+  console.log("Número ímpar");
+}
+
+//Exercicio 4
+
+let nota = 7;
+
+if (nota >= 6) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
+
+//Exercicio 5
+
+let idade = 16;
+
+if (idade >= 16) {
+  console.log("Pode votar");
+} else {
+  console.log("Não pode votar");
+}
+
+//Exercicio 6
+
+let senha = "1234";
+
+if (senha === "1234") {
+  console.log("Acesso permitido");
+} else {
+  console.log("Acesso negado");
+}
+
+//Exercicio 7
+
+let temperatura = 30;
+
+if (temperatura >= 25) {
+  console.log("Está quente");
+} else {
+  console.log("Está frio");
+}
+
+//Exercicio 8
+
+let altura = 1.45; // em metros
+let alturaMinima = 1.40;
+
+if (altura >= alturaMinima) {
+  console.log("Pode entrar no brinquedo");
+} else {
+  console.log("Não pode entrar no brinquedo");
+}
+
+//Exercicio 9
+
+let numero2 = 0;
+
+if (numero2 > 0) {
+  console.log("Positivo");
+} else if (numero < 0) {
+  console.log("Negativo");
+} else {
+  console.log("Zero");
+}
+
+//Exercicio 10
+
+let nota2 = 8;
+
+if (nota2 >= 9) {
+  console.log("Excelente");
+} else if (nota2 >= 7) {
+  console.log("Bom");
+} else if (nota2 === 6) {
+  console.log("Regular");
+} else {
+  console.log("Reprovado");
+}
+
+//Exercicio 11
+
+let idade2 = 20;
+let temCNH = true;
+
+if (idade2 >= 18 && temCNH) {
+  console.log("Pode dirigir");
+} else {
+  console.log("Não pode dirigir");
+}
+
+//Exercicio 12
+
+let valorCompra = 150;
+
+if (valorCompra > 100) {
+  console.log("Ganhou desconto");
+} else {
+  console.log("Sem desconto");
+}
+
+//Exercicio 13
+
+let usuario = "admin";
+let senha4 = "123";
+
+if (usuario === "admin" && senha4 === "123") {
+  console.log("Login realizado");
+} else {
+  console.log("Erro de login");
+}
+
+//Exercicio 14
+
+let idade3 = 15;
+
+if (idade3 >= 16) {
+  console.log("Pode assistir");
+} else {
+  console.log("Não pode assistir");
+}
+
+//Exercicio 15
+
+let cor = "verde";
+
+if (cor === "verde") {
+  console.log("Pode passar");
+} else if (cor === "amarelo") {
+  console.log("Atenção");
+} else if (cor === "vermelho") {
+  console.log("Pare");
+} else {
+  console.log("Cor inválida");
+}
